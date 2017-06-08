7448b91925315e6dce511fde5f33a6c645d050f0
scrumbock <scrumboc@yahoo.com>
2017-06-08 12:27:35 -0400

    finished mvp for project

--- app.rb
+++ app.rb
@@ -11,7 +11,6 @@ get '/'  do
 end
 
 get '/signup' do
-  @users = User.all
   erb :signup
 end
 
@@ -35,12 +34,15 @@ end
 end
 
 post '/profile' do
+user = User.find(session[:user_id])
 Blog.create(title: params[:title], category: params[:category], content: params[:content], user_id: session[:user_id])
 redirect '/profile'
 end
 
 get '/profile' do
-@blogs = Blog.where(user_id: session[:user_id])
+@user = User.find(session[:user_id])
+@blogs = @user.blogs
+# @blogs = Blog.where(user_id: session[:user_id])
 erb :profile
 end
 
@@ -59,4 +61,27 @@ post '/update_user_info' do
   user = User.find(session[:user_id])
   user.update(params[:user])
   redirect '/profile'
+end
+
+
+get '/users' do
+  @users = User.all
+  erb :users
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  @blog = @user.blogs
+  erb :user
+end
+
+get '/feed' do
+  blogs = Blog.all
+  blogs = blogs.reverse
+  if blogs.length <10
+    @blogs = blogs
+  else
+    @blogs = blogs[0, 10]
+  end
+  erb :feed
 end
